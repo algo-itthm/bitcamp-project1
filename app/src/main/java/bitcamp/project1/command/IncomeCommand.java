f46f305a41156f64ea77e83598964874a30dcb79
seulk <skbb11@naver.com>
2024-06-27 21:18:34 +0900

    seulki

--- app/src/main/java/bitcamp/project1/command/IncomeCommand.java
+++ app/src/main/java/bitcamp/project1/command/IncomeCommand.java
@@ -8,7 +8,7 @@ import bitcamp.project1.util.Prompt;
 public class IncomeCommand {
 
     LinkedList incomeList = new LinkedList();
-    String[] categorys = new String[]{"월급","용돈","기타"};
+
     public void executeIncomeCommand(String command) {
         System.out.printf("[%s]\n", command);
 
@@ -83,65 +83,80 @@ public class IncomeCommand {
     }
 
 
-
     public void updateIncome() {
         String incomeDate = Prompt.input("날짜?");
-        System.out.println("번호 항목 금액");
-
+        int count = 0;
         for (Object obj : incomeList.toArray()) {
             Income income = (Income) obj;
             if (income.getDate().equals(incomeDate)) {
+                if (count == 0) {
+                    System.out.println("번호 항목 금액");
+                }
+                count++;
                 System.out.printf("%d %s %d\n",
-                        income.getNo(), "테스트", income.getAmount());
+                        income.getNo(), "항목", income.getAmount());
             }
         }
-
-        int incomeNo = Prompt.inputInt("번호(0은 이전)?");
-        if (incomeNo == 0) {
+        if (count == 0) {
+            System.out.println("없는 날짜입니다.");
             return;
         }
 
-        Income income = (Income) incomeList.get(incomeList.indexOf(new Income(incomeNo)));
-        if (income == null) {
-            System.out.println("없는 수입입니다.");
-            return;
-        }
+        while (true) {
+            int incomeNo = Prompt.inputInt("번호(0은 이전)?");
+            if (incomeNo == 0) {
+                return;
+            }
 
-        income.setAmount(Prompt.inputInt("금액(%s)?", income.getAmount()));
+            Income income = (Income) incomeList.get(incomeList.indexOf(new Income(incomeNo)));
+            if (income == null) {
+                System.out.println("없는 수입입니다.");
+                return;
+            }
+
+            income.setAmount(Prompt.inputInt("금액(%s)?", income.getAmount()));
 //    income.setCategory(Prompt.input("분류(%s)?", income.getCategory()));
-        income.setContent(Prompt.input("항목(%s)?", income.getContent()));
-        System.out.println("변경 했습니다.");
-
+            income.setContent(Prompt.input("항목(%s)?", income.getContent()));
+            System.out.println("변경 했습니다.");
+        }
     }
 
     public void deleteIncome() {
         String incomeDate = Prompt.input("날짜?");
-        System.out.println("번호 항목 금액");
-
+        int count = 0;
         for (Object obj : incomeList.toArray()) {
             Income income = (Income) obj;
             if (income.getDate().equals(incomeDate)) {
+                if (count == 0) {
+                    System.out.println("번호 항목 금액");
+                }
+                count++;
                 System.out.printf("%d %s %d\n",
-                        income.getNo(), "테스트", income.getAmount());
+                        income.getNo(), "항목", income.getAmount());
             }
         }
-
-        int incomeNo = Prompt.inputInt("번호(0은 이전)?");
-        if (incomeNo == 0) {
+        if (count == 0) {
+            System.out.println("없는 날짜입니다.");
             return;
         }
+        while (true) {
+            int incomeNo = Prompt.inputInt("번호(0은 이전)?");
+            if (incomeNo == 0) {
+                return;
+            }
 
-        Income deletedIncome = (Income) incomeList.get(incomeList.indexOf(new Income(incomeNo)));
-        if (deletedIncome == null) {
-            System.out.println("없는 번호 입니다.");
-            return;
-        }
+            Income deletedIncome = (Income) incomeList.get(incomeList.indexOf(new Income(incomeNo)));
+            if (deletedIncome == null) {
+                System.out.println("없는 번호 입니다.");
+                return;
+            }
 
-        if (deletedIncome != null) {
-            incomeList.remove(incomeList.indexOf(deletedIncome));
-            System.out.printf("%d번 수입을 삭제 했습니다.\n", deletedIncome.getNo());
-        } else {
-            System.out.println("없는 수입입니다.");
+            if (deletedIncome != null) {
+                incomeList.remove(incomeList.indexOf(deletedIncome));
+                System.out.printf("%d번 수입을 삭제 했습니다.\n", deletedIncome.getNo());
+            } else {
+                System.out.println("없는 수입입니다.");
+            }
         }
     }
 }
