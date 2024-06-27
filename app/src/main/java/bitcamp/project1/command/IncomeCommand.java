e3933cd84ed782a87a478ae2937823be2e6abbd4
seulk <skbb11@naver.com>
2024-06-27 19:09:09 +0900

    m1

--- app/src/main/java/bitcamp/project1/command/IncomeCommand.java
+++ app/src/main/java/bitcamp/project1/command/IncomeCommand.java
@@ -9,7 +9,7 @@ import java.util.LinkedList;
 public class IncomeCommand {
     LinkedList<Income> incomeList = new LinkedList<Income>();
 
-    public void executeIncomeCommand(String command){
+    public void executeIncomeCommand(String command) {
         System.out.printf("[%s]\n", command);
 
         switch (command) {
@@ -27,7 +27,8 @@ public class IncomeCommand {
                 break;
         }
     }
-    public void addIncome(){
+
+    public void addIncome() {
         Income income = new Income();
         income.setTitle(Prompt.input("제목?"));
         income.setContent(Prompt.input("내용?"));
@@ -37,7 +38,8 @@ public class IncomeCommand {
         boardList.add(income);
         System.out.println("등록했습니다.");
     }
-    public void viewIncome(){
+
+    public void viewIncome() {
         int incomeNo = Prompt.inputInt("게시판 번호?");
         Income income = (Income) incomeList.get(incomeList.indexOf(new Income(incomeNo)));
         if (income == null) {
@@ -49,29 +51,30 @@ public class IncomeCommand {
         System.out.printf("내용 : %s\n", income.getContent());
         System.out.printf("작성일 : %1$tY-%1$tm-%1$td  %1$tH : %1$tM : %1$tS\n", income.getCreatedDate());
         System.out.printf("조회수 : %s\n", income.getViewCount());
-        }
     }
-    public void updateIncome(){
-        int incomeNo = Prompt.inputInt("게시판 번호?");
-        Income income = (Income) income.get(income.indexOf(new Income(incomeNo)));
-        if (income == null) {
-            System.out.println("없는 게시판입니다.");
-            return;
-        }
-        income.setViewCount(income.getViewCount() + 1);
-        income.setTitle(Prompt.input("게시판명(%s)\n", income.getTitle()));
-        income.setContent(Prompt.input("내용(%s)\n", income.getContent()));
-        System.out.println("변경하였습니다.");
 
+
+public void updateIncome() {
+    int incomeNo = Prompt.inputInt("게시판 번호?");
+    Income income = (Income) income.get(income.indexOf(new Income(incomeNo)));
+    if (income == null) {
+        System.out.println("없는 게시판입니다.");
+        return;
     }
-    public void deleteIncome(){
-        int incomeNo = Prompt.inputInt("게시판 번호?");
-        Income deletedIncome = (Income) boardList.get(boardList.indexOf(new Income(incomeNo)));
-        if (deletedBoard != null) {
-            boardList.remove(boardList.indexOf(deletedBoard));
-            System.out.printf("'%s' 게시판 삭제했습니다.\n", deletedBoard.getTitle());
-        } else
-            System.out.println("없는 게시판입니다.");
-    }
+    income.setViewCount(income.getViewCount() + 1);
+    income.setTitle(Prompt.input("게시판명(%s)\n", income.getTitle()));
+    income.setContent(Prompt.input("내용(%s)\n", income.getContent()));
+    System.out.println("변경하였습니다.");
 
 }
+
+public void deleteIncome() {
+    int incomeNo = Prompt.inputInt("게시판 번호?");
+    Income deletedIncome = (Income) boardList.get(boardList.indexOf(new Income(incomeNo)));
+    if (deletedBoard != null) {
+        boardList.remove(boardList.indexOf(deletedBoard));
+        System.out.printf("'%s' 게시판 삭제했습니다.\n", deletedBoard.getTitle());
+    } else
+        System.out.println("없는 게시판입니다.");
+}
+
