dbd1c6ff66c90f602e1b522a21bc5bc921b20184
seulk <skbb11@naver.com>
2024-06-27 16:49:57 +0900

    m1

--- app/src/main/java/bitcamp/project1/Test1.java
+++ app/src/main/java/bitcamp/project1/Test1.java
@@ -4,11 +4,23 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
-    static void m1(){
+
+    static void m1() {
         System.out.println("오호라!!!!");
     }
 
     static void m3() {
-      System.out.println("오호라!!!");
+        System.out.println("오호라!!!");
     }
+
+    //"income(수입)" or "expense(지출)”
+    String[] mainMenus = new String[]{"수입", "지출", "조회", "종료"};
+    String[][] subMenus = {
+            {"등록", "목록", "조회", "변경", "삭제", "이전"},
+            {"등록", "목록", "조회", "변경", "삭제", "이전"},
+            {"수입조회", "지출조회", "수입/지출 조회", "이전"},
+            {}
+    };
+
+
 }
